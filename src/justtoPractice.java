4e2942749da960098f817e5695150dca415b6a13
Annapoorna <annapoorna888.reddy@gmail.com>
2025-01-18 22:10:24 +0530

    practice conflicts

--- src/justtoPractice.java
+++ src/justtoPractice.java
@@ -1,11 +1,9 @@
 public class justtoPractice {
     public static void main(String[] args) {
-        int a=10;
-        int b-20;
-        int sum=10+20;
-        int sun=30+80;
-        int sum=10+21;
-        System.out.println("sum");
+        int a=2;
+        int b=2;
+        int c=a+b;
+        System.out.println(c);
 
     }
 }
