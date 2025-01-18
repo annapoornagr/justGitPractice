049d19326de77143e7af2f17dde25bd3e67dff1e
Annapoorna <annapoorna888.reddy@gmail.com>
2025-01-18 22:05:55 +0530

    changes again

--- src/justtoPractice.java
+++ src/justtoPractice.java
@@ -2,7 +2,6 @@ public class justtoPractice {
     public static void main(String[] args) {
         int a=10;
         int b-20;
-        int sum=10+20;
         int sun=30+80;
         System.out.println("sum");
 
