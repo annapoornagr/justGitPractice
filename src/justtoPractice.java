1da026e645ed725a90a3036ed14beac6c2004695
Annapoorna <annapoorna888.reddy@gmail.com>
2025-01-18 19:08:16 +0530

    iam commiting

--- src/justtoPractice.java
+++ src/justtoPractice.java
@@ -2,5 +2,7 @@ public class justtoPractice {
     public static void main(String[] args) {
         System.out.println("Hello, good morning");
         System.out.println("yes");
+        System.out.println("i am learning");
+        System.out.println("banglore");
     }
 }
