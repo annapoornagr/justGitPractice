daf8b7bfd8db9a860e9d1d12aede9cabb3a54437
Annapoorna <annapoorna888.reddy@gmail.com>
2025-01-18 19:45:56 +0530

    change done

--- src/justtoPractice.java
+++ src/justtoPractice.java
@@ -1,8 +1,9 @@
 public class justtoPractice {
     public static void main(String[] args) {
-        System.out.println("Hello, good morning");
-        System.out.println("yes");
-        System.out.println("i am learning");
-        System.out.println("banglore");
+        int a=10;
+        int b-20;
+        int sum=10+20;
+        System.out.println("sum");
+
     }
 }
